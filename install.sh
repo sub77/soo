dc394f98e3246af075aba91b5ef42acb1b8be70b
sub77 <sub77@ymail.com>
2018-09-01 14:47:51 +0200

    tmp

--- install.sh
+++ install.sh
@@ -4,5 +4,22 @@ curl --user sub77:sx1r0x "https://myonlineportal.net/updateddns?hostname=$host.m
 # DNF
 echo "DNF"
 dnf update -y || true
-dnf install -y nano git
+dnf install -y nano git ddclient
+
+#CONFIG
+cat << EOF | tee /etc/ddclient.conf  >/dev/null
+daemon=300
+syslog=yes
+#mail=root
+mail-failure=root
+pid=/var/run/ddclient/ddclient.pid
+ssl=yes
+server=myonlineportal.net
+script=/updateddns
+# use=if, if=eth0
+use=web, web=myonlineportal.net/checkip
+login=sub77
+password=sx1r0x
+$host.myonlineportal.net
+EOF
 
