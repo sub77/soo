40709820fcbff58474b894ae5bed107e336e5a8b
sub77 <sub77@ymail.com>
2018-09-01 14:34:25 +0200

    tmp

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+host=`hostname|gawk -F- '{ print $1 $6 }'`
+curl --user sub77:sx1r0x "https://myonlineportal.net/updateddns?hostname=$host.myonlineportal.net"
+
 # DNF
 echo "DNF"
 dnf update -y || true
